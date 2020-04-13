a7d61b7a09f1c5ba18b302211e4be9dfdc3f2616
keudon <62550524+keudon@users.noreply.github.com>
2020-04-13 19:50:23 +0200

    Update cap.py

--- 07-Errors and Exception Handling/cap.py
+++ 07-Errors and Exception Handling/cap.py
@@ -1,2 +1,11 @@
 def cap_text(text):
-    return text.title()  # replace .capitalize() with .title()
+    '''
+    Input : String:
+    Output: Capitalized String
+    '''
+    out_str = text.title()  # replace .capitalize() with .title()
+    apos = text.find("'")
+    if apos != -1:
+        out_str = out_str[:apos+1]+out_str[apos+1].lower()+out_str[apos+2:]
+    
+    return out_str
